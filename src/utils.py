82a188a89657570c013400755f85a9d65a0e0fcc
dynamicsamic <mirabyansr@gmail.com>
2024-09-13 16:10:41 +0300

    refactor: add type annotations

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import asyncio
 from concurrent.futures import ProcessPoolExecutor
+from typing import Any, Awaitable, Callable
 
 
-async def run_in_executor(f, *args):
+async def run_in_executor(f: Callable, *args: Any) -> Awaitable:
     loop = asyncio.get_running_loop()
     executor = ProcessPoolExecutor(2)
     awaitable = loop.run_in_executor(executor, f, *args)
